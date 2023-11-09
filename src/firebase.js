2d83f1b671aec635fde5c03a496d6215ad8c091a
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-11-09 16:57:27 +0330

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIu7a_ZJI_8MiT5Js-j0D8tiCjiykBcbI",
+  authDomain: "reactify-f0dc5.firebaseapp.com",
+  projectId: "reactify-f0dc5",
+  storageBucket: "reactify-f0dc5.appspot.com",
+  messagingSenderId: "665197616908",
+  appId: "1:665197616908:web:625cdbaad50d8c02217329",
+};
+
+const app = initializeApp(firebaseConfig);
