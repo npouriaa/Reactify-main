f3781db6d94d3911d4ee323548bf576bc4474e10
pouria <npouriaa@gmail.com>
2024-02-08 16:41:45 +0330

    update ScrollElement.jsx

--- src/components/ScrollElement.jsx
+++ src/components/ScrollElement.jsx
@@ -3,7 +3,7 @@ import { useInView } from "react-intersection-observer";
 
 const ScrollElement = ({ children , styles}) => {
   const { ref, inView } = useInView({
-    triggerOnce: true,
+    triggerOnce: false,
   });
 
   return (
