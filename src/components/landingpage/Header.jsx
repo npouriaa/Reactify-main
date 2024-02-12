31c11c1120bee69c136efd44870db1fe39854752
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-02-12 13:29:49 +0330

    add another beakpoint for responsive design (3xl added)

--- src/components/landingpage/Header.jsx
+++ src/components/landingpage/Header.jsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="bg-[url(assets/images/landingpage/header/bg.png)] gap-10 max-sm:h-[600px] md:h-[900px] lg:h-[900px] xl:h-[860px] 2xl:h-[1000px] max-sm:bg-cover lg:bg-contain relative items-center bg-no-repeat flex flex-col  overflow-hidden">
+    <header className="bg-[url(assets/images/landingpage/header/bg.png)] gap-10 max-sm:h-[600px] md:h-[900px] lg:h-[900px] xl:h-[860px] 2xl:h-[1000px] 3xl:h-[1200px] max-sm:bg-cover lg:bg-contain relative items-center bg-no-repeat flex flex-col  overflow-hidden">
       <Nav />
       <div className=" max-sm:w-full p-4 lg:w-5/6 xl:w-3/4 2xl:w-full 2xl:justify-center relative h-[550px] flex md:flex-col 2xl:flex-row items-center gap-10">
         <div className="md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[500px] flex flex-col gap-6 text-white h-[361px] p-2">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,12 @@ export default {
   theme: {
     screens: {
       'max-sm': '360px',
-
       'sm': '481px',
-
       'md': '641px',
-
       'lg': '961px',
-
       'xl': '1025px',
-
       '2xl': '1250px',
+      '3xl': '1400px',
     },
     extend: {},
   },
