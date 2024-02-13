0bb3a7508b57a8847037f3373beb561fb206cf32
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-02-13 13:23:51 +0330

    change in register form

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,43 +1,50 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo/Reactify-black.png";
-import { Button, Form, Input, Upload } from "antd";
+import { Button, Form, Input, Tooltip, Upload } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
+
 
 const Register = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-tr from-[#7932F5] via-[#F5658C] to-[#F5658C]">
-      <div className="flex gap-4 items-center flex-col rounded-2xl max-sm:w-[350px] sm:w-[440px] sm2:w-[380px] md:w-[500px] shadow-xl bg-white py-6 px-8">
+      <div className="flex gap-3 items-center flex-col rounded-2xl max-sm:w-[350px] sm:w-[440px] sm2:w-[380px] md:w-[500px] shadow-xl bg-white py-6 px-8">
         <div className="w-full flex items-center justify-center relative">
           <Link to="/">
             <img className="max-sm:h-10 sm:h-12" src={logo} alt="logo" />
           </Link>
-          <Link
-            to="/"
-            className="absolute left-0 w-9 h-9 bg-[#7932F5] hover:bg-[#FB3C7F] transition-all flex justify-center items-center rounded-full"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2.5}
-              stroke="#fff"
-              className="w-5 h-5"
+          <Tooltip title="home page">
+            <Link
+              to="/"
+              className="absolute left-0 w-9 h-9 bg-[#7932F5] hover:bg-[#FB3C7F] transition-all flex justify-center items-center rounded-full"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
-              />
-            </svg>
-          </Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2.5}
+                stroke="#fff"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+                />
+              </svg>
+            </Link>
+          </Tooltip>
         </div>
         <div className="w-full text-center flex flex-col justify-center items-center">
-          <h1 className="text-xl">Welcome!</h1>
-          <p className="text-gray-700">
+          <h1 className="max-sm:text-lg md:text-xl">Welcome!</h1>
+          <p className="text-gray-700 max-sm:text-sm md:text-base">
             Join us to connect, share, and discover.
           </p>
         </div>
-        <Form className="max-sm:w-full md:w-5/6 flex flex-col gap-2" layout="vertical" name="register_form">
+        <Form
+          className="max-sm:w-full md:w-5/6 flex flex-col gap-2"
+          layout="vertical"
+          name="register_form"
+        >
           <Form.Item
             hasFeedback
             label="username :"
@@ -115,18 +122,26 @@ const Register = () => {
             rules={[
               {
                 required: true,
-                message: "Please select a profile photo!",
+                message: "Please select a avatar!",
               },
             ]}
           >
-            <Upload maxCount={1} name="profile" beforeUpload={() => false}>
-              <Button className="antd-btn border-none bg-[#7932F5] text-white hover:bg-[#FB3C7F] transition-all duration-500" icon={<UploadOutlined />}>Click to upload</Button>
+            <Upload maxCount={1} name="avatar" beforeUpload={() => false}>
+              <Button
+                className="antd-btn border-none bg-[#7932F5] text-white hover:bg-[#FB3C7F] transition-all duration-500"
+                icon={<UploadOutlined />}
+              >
+                Click to upload
+              </Button>
             </Upload>
           </Form.Item>
           <div className="w-full flex items-center justify-center">
-            <button className="w-full bg-[#7932F5] text-white hover:bg-[#FB3C7F] transition-all duration-500 py-2 rounded-lg">Register</button>
+            <button className="w-full bg-[#7932F5] text-white hover:bg-[#FB3C7F] transition-all duration-500 py-2 rounded-lg">
+              Register
+            </button>
           </div>
         </Form>
+        <p className="text-sm">already have an account ? <Link className="text-[#7932F5] hover:text-[#FB3C7F] transition-all duration-500" to='/login'>login</Link></p>
       </div>
     </div>
   );
