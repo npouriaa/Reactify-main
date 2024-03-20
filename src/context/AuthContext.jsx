f94d47e72cd10063ca88aa495bf97e8b1cac440b
pouria <npouriaa@gmail.com>
2024-03-20 14:02:51 +0330

    update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,24 @@
-import { createContext, useState } from "react";
+import { onAuthStateChanged } from "@firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
 
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [registerInfo, setRegistetrInfo] = useState({});
+
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user)
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <AuthContext.Provider value={{ registerInfo, setRegistetrInfo }}>
