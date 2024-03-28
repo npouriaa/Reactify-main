0d077921f5a601102833dc1d3a4c059a1e6a431e
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-03-28 17:48:45 +0330

    add logout user function

--- src/Pages/UserPages/Home.jsx
+++ src/Pages/UserPages/Home.jsx
@@ -1,18 +1,27 @@
 import { useContext } from "react";
 import { RequestsContext } from "../../context/RequestsContext";
 import LoaderModal from "../../components/LoaderModal";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const { loading } = useContext(RequestsContext);
+  const { loading, setLoading } = useContext(RequestsContext);
+  const navigate = useNavigate();
 
-  if (loading) {
-    return <LoaderModal />;
-  }
+  const logOutUser = async () => {
+    setLoading(true);
+    await auth.signOut();
+    localStorage.removeItem("accessToken");
+    setLoading(false);
+    navigate("/login");
+  };
 
   return (
-    <div>
+    <div className="w-screen h-screen flex justify-center items-center flex-col">
+      {loading && <LoaderModal />}
       <p>Home</p>
       <h1>logged in</h1>
+      <button onClick={() => logOutUser()}>logout</button>
     </div>
   );
 };
