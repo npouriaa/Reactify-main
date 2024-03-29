4353e6e87333465cd9dba161fca2f7aac2a68c34
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-03-29 11:38:47 +0330

    update verifyEmail.jsx and App.css

--- src/Pages/UserPages/VerifyEmail.jsx
+++ src/Pages/UserPages/VerifyEmail.jsx
@@ -104,52 +104,50 @@ const VerifyEmail = () => {
   }
 
   return (
-    <>
+    <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-tr from-[#7932F5] via-[#F5658C] to-[#F5658C] verify-email">
       {contextHolder}
       {modalContextHolder}
-      <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-tr from-[#7932F5] via-[#F5658C] to-[#F5658C]">
-        {loading ? (
-          <LoaderModal />
-        ) : (
-          <div className="max-sm:w-[330px] sm2:w-[360px] sm:w-[460px] md:w-[600px] max-sm:py-8 max-sm:px-5 sm:py-5 sm:px-6 md:px-8 md:py-6 gap-2 bg-white rounded-2xl shadow-xl flex flex-col justify-center items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              className="w-16 h-16 stroke-[#F5658C]"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
-              />
-            </svg>
-            <h1 className="text-2xl font-bold">Check your email</h1>
-            <div className="max-sm:text-sm md:text-base text-center">
-              <p>Your registeration is complete ! </p>
-              <p>
-                we've sent an email with a verification link to{" "}
-                {auth?.currentUser?.email} please confirm it to start using the
-                app.
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="max-sm:text-sm text-gray-500">
-                Haven't received the code yet ?
-              </p>
-              <button
-                disabled={timer ? true : false}
-                onClick={() => resendVerificationLink()}
-                className="max-sm:text-sm md:text-base p-2 bg-[#7932F5] text-white hover:bg-[#FB3C7F] disabled:bg-red-500 transition-all duration-500 rounded-lg"
-              >
-                Resend link {timer ? `in ${timer}` : ""}
-              </button>
-            </div>
+      {loading ? (
+        <LoaderModal />
+      ) : (
+        <div className="max-sm:w-[330px] sm2:w-[360px] sm:w-[460px] md:w-[600px] max-sm:py-8 max-sm:px-5 sm:py-5 sm:px-6 md:px-8 md:py-6 gap-2 bg-white rounded-2xl shadow-xl flex flex-col justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            className="w-16 h-16 stroke-[#F5658C]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
+          </svg>
+          <h1 className="text-2xl font-bold">Check your email</h1>
+          <div className="max-sm:text-sm md:text-base text-center">
+            <p>Your registeration is complete ! </p>
+            <p>
+              we've sent an email with a verification link to{" "}
+              {auth?.currentUser?.email} please confirm it to start using the
+              app.
+            </p>
           </div>
-        )}
-      </div>
-    </>
+          <div className="flex flex-col gap-1">
+            <p className="max-sm:text-sm text-gray-500">
+              Haven't received the code yet ?
+            </p>
+            <button
+              disabled={timer ? true : false}
+              onClick={() => resendVerificationLink()}
+              className="max-sm:text-sm md:text-base p-2 bg-[#7932F5] text-white hover:bg-[#FB3C7F] disabled:bg-red-500 transition-all duration-500 rounded-lg"
+            >
+              Resend link {timer ? `in ${timer}` : ""}
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
