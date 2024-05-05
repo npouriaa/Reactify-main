968cedd8ec51094c77675fbcfa200d7df9a3fe93
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-05-05 21:05:03 +0330

    add feed banner component

--- src/Pages/UserPages/Home.jsx
+++ src/Pages/UserPages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import useNotification from "../../Hooks/useNotification";
 import { IoSearchOutline } from "react-icons/io5";
+import FeedBanner from "../../components/Home/FeedBanner";
 
 const Home = () => {
   // const { openNotificationSuccess, contextHolder } = useNotification();
@@ -10,18 +11,19 @@ const Home = () => {
   // }, []);
 
   return (
-    <div className="w-full h-full flex justify-center items-start p-8">
-      <div className="flex max-sm:w-full sm2:w-5/6 md2:w-3/4 xl:w-3/5">
+    <div className="w-full h-full flex justify-center items-start max-sm:px-4 max-sm:py-8">
+      <div className="flex flex-col gap-5 max-sm:w-full sm2:w-5/6 md2:w-3/4 4xl:w-3/5">
         <div className="max-sm:flex md:hidden w-full h-12 px-3 py-[.2rem] bg-white items-center justify-between rounded-3xl overflow-hidden">
           <input
             placeholder="Search for friends..."
             className="w-[95%] py-2 px-2 text-sm"
             type="text"
           />
-          <button className="">
+          <button>
             <IoSearchOutline color="#7932F5" size={25} />
           </button>
         </div>
+        <FeedBanner />
       </div>
     </div>
   );
--- src/components/Home/FeedBanner.jsx
+++ src/components/Home/FeedBanner.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import bg from "../../assets/images/user-home/feed.png";
+import people from "../../assets/images/user-home/people.png";
+import { IoMdMegaphone } from "react-icons/io";
 
 const FeedBanner = () => {
-  return <div className="max-sm:w-full "></div>;
+  return (
+    <div className="w-full relative max-sm:h-32 md:h-52 rounded-md bg-gradient-to-l from-[#ffea00] to-[#ff9800] flex justify-center items-center">
+      <img
+        className="h-full object-cover w-full -z-1"
+        src={bg}
+        alt="feed-banner"
+      />
+      <div className="absolute flex justify-center items-center w-full h-full">
+        <div className="w-full overflow-hidden flex gap-4 p-4">
+          <div className="w-20 max-sm:hidden sm:flex items-center relative justify-center after:w-[3px] after:rounded-xl after:h-[5rem]  after:right-0 after:absolute after:bg-white">
+            <IoMdMegaphone className="text-white relative min-h-[3.5rem] min-w-[3.5rem] max-w-[3.5rem] max-h-[3.5rem]" />
+          </div>
+          <div className="h-full text-white max-sm:text-center sm:text-start w-full flex justify-center flex-col">
+            <h3 className="font-bold text-2xl">Members Newsfeed</h3>
+            <p>Check what your friends have been up to!</p>
+          </div>
+          <img
+            className=" max-sm:hidden lg:block lg:w-1/2 3xl:w-auto absolute right-0 bottom-0 object-cover"
+            src={people}
+            alt="feed-banner-poeple"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedBanner;
