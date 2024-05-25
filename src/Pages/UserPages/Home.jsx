cec7742860b39bfcd9b6a207adca61443306b039
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-05-25 12:37:24 +0330

    update Home.jsx

--- src/Pages/UserPages/Home.jsx
+++ src/Pages/UserPages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
   return (
     <div className="w-full h-full flex justify-center items-start max-sm:px-4 max-sm:py-8">
-      <div className="flex flex-col gap-5 max-sm:w-full sm2:w-5/6 md2:w-3/4 xl:w-3/5">
+      <div className="flex flex-col gap-5 max-sm:w-full sm2:w-5/6 md2:w-3/4 2xl:w-3/5">
         <div className="max-sm:flex md:hidden w-full h-12 px-3 py-[.2rem] bg-white items-center justify-between rounded-3xl overflow-hidden">
           <input
             placeholder="Search for friends..."
