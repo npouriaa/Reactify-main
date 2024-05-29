57a6d4f57da3cb352b840123fe1ba3e9d9e302dd
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-05-29 20:05:12 +0330

    add edit profile header image btn

--- src/components/user/ProfileHeader.jsx
+++ src/components/user/ProfileHeader.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { RequestsContext } from "../../context/RequestsContext";
+import { GoPencil } from "react-icons/go";
 
 const ProfileHeader = () => {
   const { currentUser } = useContext(RequestsContext);
-  const joinDate = currentUser?.metadata.creationTime.split(" ")
+  const joinDate = currentUser?.metadata.creationTime.split(" ");
 
   return (
     <div className="w-full rounded-lg max-sm:h-[23rem] sm:h-[20rem] md:h-[19rem] lg:h-[17rem] bg-[url('../../assets/images/user/banner.jpg')]">
-      <div className="flex w-full h-full rounded-lg user-banner-shadow max-sm:items-center lg:items-end">
+      <div className="flex w-full h-full rounded-lg user-banner-shadow max-sm:items-center lg:items-end relative">
         <div className="flex items-center justify-between w-full gap-4 px-8 text-white max-sm:flex-col lg:flex-row max-sm:h-3/4">
           <div className="flex items-center gap-4 p-2 max-sm:flex-col lg:flex-row">
             <div className="relative cursor-pointer h-28 w-28 p-[10px] after:absolute after:bg-cover after:w-full after:h-full after:top-0 after:right-0 after:bg-[url('../../assets/images/user/border-profile-header.png')] after:rotate-0 after:transition-all after:ease-in-out hover:after:rotate-[30deg]">
@@ -19,7 +20,9 @@ const ProfileHeader = () => {
             <div className="flex flex-col max-sm:text-center lg:text-start">
               <h3 className="text-xl capitalize">{currentUser?.displayName}</h3>
               <h3 className="text-lg capitalize">Iran / Sabzevar</h3>
-              <h5 className="text-sm text-[#d7d7d7] capitalize">joined {joinDate[2]} {joinDate[3]}</h5>
+              <h5 className="text-sm text-[#d7d7d7] capitalize">
+                joined {joinDate[2]} {joinDate[3]}
+              </h5>
             </div>
           </div>
           <div className="flex justify-center flex-wrap gap-4 text-[.9rem] text-[#d7d7d7] max-sm:items-center lg:items-end h-[3.4rem]">
@@ -34,6 +37,9 @@ const ProfileHeader = () => {
             </p>
           </div>
         </div>
+        <button className="absolute top-7 right-7 p-2 rounded-full bg-white">
+          <GoPencil className="w-6 h-6" />
+        </button>
       </div>
     </div>
   );
