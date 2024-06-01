d36cc7590b9f067b59fc3a2c4bd6a05fe6d72178
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-06-01 13:05:50 +0330

    make AboutMe and  AddPost responsive

--- src/Pages/UserPages/UserProfile.jsx
+++ src/Pages/UserPages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProfileHeader from "../../components/user/userProfile/ProfileHeader";
 import AddPost from "../../components/user/userProfile/AddPost";
+import AboutMe from "../../components/user/userProfile/AboutMe";
 
 const UserProfile = () => {
   return (
@@ -8,8 +9,9 @@ const UserProfile = () => {
       <div className="flex flex-col gap-5 max-sm:w-full sm2:w-5/6 md2:w-3/4 2xl:w-3/5">
         <ProfileHeader />
         <div className="w-full flex flex-col ">
-          <div className="w-full">
+          <div className="w-full flex gap-4 max-sm:flex-col lg:flex-row">
             <AddPost />
+            <AboutMe />
           </div>
         </div>
       </div>
---
+++ src/components/user/userProfile/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className="rounded-md bg-white max-sm:order-1 lg:order-2 max-sm:w-full lg:w-1/3 h-[350px] p-4">
+      <div className="w-full"></div>
+    </div>
+  );
+};
+
+export default AboutMe;
