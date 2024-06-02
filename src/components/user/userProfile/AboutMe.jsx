250e5b23ef2b802f331aa5cff4310a79e327f084
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-06-02 16:47:19 +0330

    add inputs to edit user "about me" section , update App.css

--- src/components/user/userProfile/AboutMe.jsx
+++ src/components/user/userProfile/AboutMe.jsx
@@ -1,36 +1,66 @@
 import { GoPencil } from "react-icons/go";
 import { RequestsContext } from "../../../context/RequestsContext";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { FaInstagram } from "react-icons/fa6";
 import { FaTelegram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { Modal } from "antd";
+import { Modal, Form, Input, AutoComplete } from "antd";
 
 const AboutMe = () => {
   const { currentUser } = useContext(RequestsContext);
   const joinDate = currentUser?.metadata.creationTime.split(" ");
   const [open, setOpen] = useState(false);
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+  const frmRef = useRef();
+
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(
+        [".com", ".ir", ".dev", ".org", ".net"].map(
+          (domain) => `${value}${domain}`
+        )
+      );
+    }
+  };
+
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
 
   const showModal = () => {
     setOpen(true);
+    frmRef.current?.resetFields();
   };
-  
+
   const handleOk = () => {
     setOpen(false);
+    frmRef.current?.resetFields();
   };
 
   const handleCancel = () => {
     setOpen(false);
+    frmRef.current?.resetFields();
   };
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="flex flex-col gap-4 rounded-md bg-white max-sm:order-1 lg:order-2 max-sm:w-full lg:w-2/5 xl:w-1/3 px-7 py-5">
       <div className="w-full h-9 flex item-center justify-between">
         <h3 className="text-[1.05rem] relative after:absolute after:bottom-0 after:left-0 after:rounded-md after:h-1 after:w-3 after:bg-[#615DFA] before:absolute before:bottom-0 before:left-4 before:h-1 before:rounded-md before:w-6 before:bg-[#615DFA]">
           About Me
         </h3>
-        <button onClick={showModal} className="p-2 shadow-3xl rounded-full bg-white hover:bg-[#d7d7d7] transition-all duration-300">
+        <button
+          onClick={showModal}
+          className="p-2 shadow-3xl rounded-full bg-white hover:bg-[#d7d7d7] transition-all duration-300"
+        >
           <GoPencil className="w-5 h-5" />
         </button>
       </div>
@@ -50,7 +80,7 @@ const AboutMe = () => {
             Email : <Link className="text-black">{currentUser?.email}</Link>
           </p>
           <p className="font-normal">
-            Phone : <span className="text-black">+989395362203</span>
+            Phone : <Link className="text-black">+989395362203</Link>
           </p>
           <p className="font-normal">
             Country : <span className="text-black">Iran</span>
@@ -102,7 +132,52 @@ const AboutMe = () => {
         )}
       >
         <div className="flex flex-col gap-3">
-          
+          <Form
+            onFinish={onFinish}
+            ref={frmRef}
+            className=" flex flex-col"
+            layout="vertical"
+            name="register_form"
+          >
+            <Form.Item label="About me :" name="aboutText">
+              <Input.TextArea
+                placeholder="Write about yourself"
+                rows={3}
+                maxLength={500}
+                count={{
+                  show: true,
+                  max: 500,
+                }}
+              />
+            </Form.Item>
+            <div className="w-full justify-between flex items-center flex-wrap">
+              <Form.Item
+                className="w-[48%]"
+                label="Phone number:"
+                name="phoneNumber"
+                rules={[
+                  {
+                    pattern: /^\d+$/,
+                    message: "Please input only numbers!",
+                  },
+                ]}
+              >
+                <Input placeholder="+989124208975" />
+              </Form.Item>
+              <Form.Item className="w-[48%]" label="Location :" name="Location">
+                <Input placeholder="Iran" />
+              </Form.Item>
+              <Form.Item className="w-[48%]" label="Web :" name="Web">
+                <AutoComplete
+                  options={websiteOptions}
+                  onChange={onWebsiteChange}
+                  placeholder="website"
+                >
+                  <Input />
+                </AutoComplete>
+              </Form.Item>
+            </div>
+          </Form>
         </div>
       </Modal>
     </div>
