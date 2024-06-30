d4b50b415c62967d336c224ef7bf1e820cbe4ebe
pouria <npouriaa@gmail.com>
2024-06-30 15:19:46 +0330

    update RequestsLoader.jsx

--- src/components/user/RequestsLoader.jsx
+++ src/components/user/RequestsLoader.jsx
@@ -3,7 +3,7 @@ import "../../assets/styles/loader.css";
 const RequestsLoader = () => {
   return (
     <div className="w-6 flex justify-center items-center">
-      <span class="requests-loader w-5 h-5 inline-block border-b-transparent border-[2px] rounded-full border-[#615DFA]"></span>
+      <span className="requests-loader w-5 h-5 inline-block border-b-transparent border-[2px] rounded-full border-[#615DFA]"></span>
       
     </div>
   );
