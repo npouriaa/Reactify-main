9ee3a149e2135d95697c8f50e99a90d5e90f6a3d
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-07-05 16:28:16 +0330

    update NoPosts.jsx

--- src/components/user/NoPosts.jsx
+++ src/components/user/NoPosts.jsx
@@ -2,11 +2,11 @@ import { CiCamera } from "react-icons/ci";
 
 const NoPosts = ({text}) => {
   return (
-    <div className="h-80 w-full dark:text-white rounded-md dark:border-white border-black border-1 border flex justify-center items-center gap-4 flex-col">
-      <div className="w-24 h-24 dark:border-white border-black border-1 border rounded-full flex justify-center items-center">
+    <div className="h-80 w-full dark:text-white rounded-md dark:border-white border-[#898989] border-1 border flex justify-center items-center gap-4 flex-col">
+      <div className="w-24 h-24 dark:border-white border-[#898989] border-1 border rounded-full flex justify-center items-center text-[#898989] dark:text-white">
         <CiCamera size={60}/>
       </div>
-      <h3 className="text-xl font-bold">{text}</h3>
+      <h3 className="text-xl font-bold text-[#898989] dark:text-white">{text}</h3>
     </div>
   );
 };
