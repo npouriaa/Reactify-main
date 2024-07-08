4cd5824dacd077f6dc3ef688ac94c91760ac0328
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-07-08 18:18:02 +0330

    remobe unused import

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,6 +1,6 @@
 import "@vidstack/react/player/styles/default/theme.css";
 import "@vidstack/react/player/styles/default/layouts/video.css";
-import { MediaPlayer, MediaProvider, Poster } from "@vidstack/react";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import {
   defaultLayoutIcons,
   DefaultVideoLayout,
